0922a1191f03b1a349cf5a78a936dcd69170fe35
osukaru16 <osukaru16@gmail.com>
2016-10-19 15:47:52 +0200

    Update Ejercicio14.py

--- Ejercicios en Python3/Ejercicio14.py
+++ Ejercicios en Python3/Ejercicio14.py
@@ -1,3 +1,20 @@
-#Ejercicio15
-for i in range(1, 6):
-	print ("Estamos estudiando metodologia de la programacion")
+#Ejercicio14 Ver 1
+try:
+	num = int(input("Introduce el valor bruto del articulo: "))
+	#num = 2236
+	resCien = int(num / 100)
+	resCincuenta = int((num-resCien*100)/50)
+	resVeinte = int(((num-resCien*100)-resCincuenta*50)/20)
+	resDiez = int((((num-resCien*100)-resCincuenta*50)-resVeinte*20)/10)
+	resCinco = int(((((num-resCien*100)-resCincuenta*50)-resVeinte*20)-resDiez*10)/5)
+	resDos = int((((((num-resCien*100)-resCincuenta*50)-resVeinte*20)-resDiez*10)-resCinco*5)/2)
+	resUno = int(((((((num-resCien*100)-resCincuenta*50)-resVeinte*20)-resDiez*10)-resCinco*5)-resDos*2)/1)
+	print("De cien hay ",resCien," billetes")
+	print("De cincuenta hay ",resCincuenta," billetes")
+	print("De veinte hay ",resVeinte," billetes")
+	print("De diez hay ",resDiez," billetes")
+	print("De cinco hay ",resCinco," billetes")
+	print("De dos hay ",resDos," monedas")
+	print("De un hay ",resUno," monedas")
+except:
+	print("ERROR: El dato introducido no era un numero entero")
